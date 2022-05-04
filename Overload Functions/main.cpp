5247a9cde7fe3c025afd199f179f7d44344914e3
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-05-04 08:29:18 +0300

    Testing creating a branch

--- Overload Functions/main.cpp
+++ Overload Functions/main.cpp
@@ -175,10 +175,11 @@ void do_all(T* array, int array_size, int shift, bool isChar)
 	cout << endl;
 }
 
-void main()
+void main() //
 {
 	setlocale(LC_ALL, "");
 	int array_size, shift;
+
 	cout << "Введите размер массива: "; cin >> array_size;
 	shift = rand() % array_size;
 	
